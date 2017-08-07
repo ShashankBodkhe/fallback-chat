14054c3605788867fd3b1f25ed3ff1d1d9876734
yogesh <yogesh.patki@gmail.com>
2017-08-07 22:35:28 +0530

    api.ai integration started

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const apiai = require('apiai');
+const apiai_client = apiai(process.env.CLIENT_API_KEY);
 
-/* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('api works');
+router.post('/', (req, res) => {
+  let query = req.body.query;
+  let response = processRequest(query,(response) => {
+    res.send(response);
+  });
 });
+
+function processRequest(query,callback) {
+
+let request = apiai_client.textRequest(query , {
+  sessionId: 'testSession'
+});
+ 
+request.on('response', (response) => {
+  callback(response.result);
+});
+ 
+request.on('error', (error) => {
+  callback("something went wrong");
+});
+ 
+request.end();
+
+}
 
 module.exports = router;
